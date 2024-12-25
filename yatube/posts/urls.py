a777b3d512c865eefbbdde5f39a329b9df727016
Максим Сапожников <silinmaksim96@gmail.com>
2024-12-25 18:06:17 +0300

    Добавлены urls и view-функции

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Главная страница
     path('', views.index),
+    #Страница для постов
     path('group/<slug:slug>/', views.group_posts),
 ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('posts.urls')),
-    path('', include('group.urls')),
     path('admin/', admin.site.urls),
 ]
